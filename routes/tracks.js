5024fa83af5911cd62d2ba1be714b55dcabf67b5
Jesus Jo <jesusjho26@gmail.com>
2022-06-02 15:40:02 -0700

    tracks routes protected with roles and auth middlewares

--- routes/tracks.js
+++ routes/tracks.js
@@ -9,6 +9,8 @@ const {
     deleteItem,
   } = require("../controllers/tracks");
 
+const checkRole = require("../middlewares/role");  
+
 const {
     validatorCreateItem,
     validatorGetItem,
@@ -16,19 +18,22 @@ const {
 
 
 //TODO http://localhost/tracks GET, POST, DELETE, PUT
-
+//TODO get all items 
 router.get('/', authMiddleware, getItems); 
-
-router.get("/:id", validatorGetItem, getItem);
-
-router.post('/', validatorCreateItem, createItem); 
-
+//TODO get only one item by id
+router.get("/:id", authMiddleware, validatorGetItem, getItem);
+//TODO create a new item
+router.post('/', authMiddleware, checkRole(["admin"]),validatorCreateItem, createItem); 
+//TODO update an item by id
 router.put(
   "/:id",
+  authMiddleware,
+  checkRole(["admin"]),
   validatorGetItem,
   validatorCreateItem,
   updateItem
 );
-router.delete("/:id", validatorGetItem, deleteItem);
+//TODO soft delete an item by id
+router.delete("/:id", authMiddleware, checkRole(["admin"]),validatorGetItem, deleteItem);
 
 module.exports = router; 
