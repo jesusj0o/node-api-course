cb8b429b1557378a8a8ae72eba0bbd464f2312ab
Jesus Jo <jesusjho26@gmail.com>
2022-06-03 11:22:32 -0700

    Swagger documentation complete for all routes

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 require("dotenv").config();
 const express = require("express"); 
 const cors = require("cors"); 
+const swaggerUI = require("swagger-ui-express");
 const dbConnect = require("./config/mongo"); 
-
+const openApiConfig = require("./docs/swagger");
 
 const app = express(); 
 app.use(cors()); 
 app.use(express.json());
 
 const PORT = process.env.PORT || 3001; 
+
+//TODO documentation routes
+app.use('/documentation', swaggerUI.serve, swaggerUI.setup(openApiConfig));
 
 //TODO getting routes 
 app.use("/api", require("./routes"));
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,55 @@ const router =  express.Router();
 const {registerCtrl, loginCtrl } = require("../controllers/auth")
 const { validatorRegister, validatorLogin} = require("../validators/auth")
 
+/** 
+ * http:localhost:3001/api/auth/register
+ *
+ * Route register new user 
+ * @openapi
+ * /auth/register: 
+ *      post: 
+ *          tags:
+ *              - auth 
+ *          summary: "Register a new user"
+ *          description: "this route is used to sign up a new user"
+ *          requestBody: 
+ *              content:
+ *                  application/json: 
+ *                      schema:
+ *                          $ref: "#/components/schemas/authRegister"
+ *          responses: 
+ *              '200': 
+ *                  description: "User has been registered successfully"
+ *              '401': 
+ *                  description: "Error by user validation"
+ * */
+
 //TODO http:localhost:3001/api/auth/register
-router.post("/register", validatorRegister, registerCtrl)
+router.post("/register", validatorRegister, registerCtrl);
+
+
+/** 
+ * http:localhost:3001/api/auth/login
+ *
+ * Route register new user 
+ * @openapi
+ * /auth/login: 
+ *      post: 
+ *          tags:
+ *              - auth 
+ *          summary: "Sign in with an existent user"
+ *          description: "this route is used to sign in with your user credentials"
+ *          requestBody: 
+ *              content:
+ *                  application/json: 
+ *                      schema:
+ *                          $ref: "#/components/schemas/authLogin"
+ *          responses: 
+ *              '200': 
+ *                  description: "User has been logged in successfully"
+ *              '401': 
+ *                  description: "Error by user credentials"
+ * */
 
 //TODOhttp:localhost:3001/api/auth/login
 router.post("/login", validatorLogin, loginCtrl);
